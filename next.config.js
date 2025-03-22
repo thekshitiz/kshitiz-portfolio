ccc52d7f16561a37515836715a2d479205b640bb
Kshitiz Rai <thekshitizrai@gmail.com>
2025-03-22 13:59:03 +0545

    updated package

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
     images: {
         formats: ['image/avif', 'image/webp'],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
